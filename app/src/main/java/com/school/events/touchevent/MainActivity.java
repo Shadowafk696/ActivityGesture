9f5a1d67b3c907dcf5a102126ce67f30cb905882
Shadowafk696 <al02689180@gmail.com>
2017-08-24 20:49:14 -0500

    Lo logre :D

--- app/src/main/java/com/school/events/touchevent/MainActivity.java
+++ app/src/main/java/com/school/events/touchevent/MainActivity.java
@@ -1,37 +1,55 @@
 package com.school.events.touchevent;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 
-public class GestureActivity extends AppCompatActivity implements
+public class MainActivity extends AppCompatActivity implements
         GestureDetector.OnGestureListener,
         GestureDetector.OnDoubleTapListener{
+
 
     private static final String DEBUG_TAG = "Gestures";
     private GestureDetectorCompat mDetector;
 
+    // Called when the activity is first created.
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesture);
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
         mDetector = new GestureDetectorCompat(this,this);
+        // Set the gesture detector as the double tap
+        // listener.
         mDetector.setOnDoubleTapListener(this);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event){
         this.mDetector.onTouchEvent(event);
+
         // Be sure to call the superclass implementation
         return super.onTouchEvent(event);
     }
 
     @Override
     public boolean onDown(MotionEvent event) {
+        changeBackgroundColor(Color.LTGRAY);
+
+        TextView textView = (TextView) findViewById(R.id.hello_gesture);
+        textView.setText(" Down 6: ");
         Log.d(DEBUG_TAG,"onDown: " + event.toString());
         return true;
     }
@@ -45,44 +63,89 @@ public class GestureActivity extends AppCompatActivity implements
 
     @Override
     public void onLongPress(MotionEvent event) {
+        changeBackgroundColor(Color.RED);
         TextView textView = (TextView) findViewById(R.id.hello_gesture);
         textView.setText("Long Press :v");
+
         Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
     }
 
     @Override
     public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
                             float distanceY) {
+        changeBackgroundColor(Color.RED);
         Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
         return true;
     }
 
     @Override
     public void onShowPress(MotionEvent event) {
+        changeBackgroundColor(Color.GREEN);
+
+
+        TextView textView = (TextView) findViewById(R.id.hello_gesture);
+        textView.setText(" Press :D");
+
         Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
     }
 
     @Override
     public boolean onSingleTapUp(MotionEvent event) {
+        changeBackgroundColor(Color.MAGENTA);
+        TextView textView = (TextView) findViewById(R.id.hello_gesture);
+        textView.setText(" UP  :( ");
         Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
         return true;
     }
 
     @Override
     public boolean onDoubleTap(MotionEvent event) {
+        changeBackgroundColor(Color.BLUE);
         Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
         return true;
     }
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent event) {
+        changeBackgroundColor(Color.CYAN);
+
+        TextView textView = (TextView) findViewById(R.id.hello_gesture);
+        textView.setText(" Double Tap Event >:v ");
         Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
         return true;
     }
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent event) {
+        changeBackgroundColor(Color.YELLOW);
+
+        TextView textView = (TextView) findViewById(R.id.hello_gesture);
+        textView.setText(" Single -.-");
         Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
         return true;
+    }
+
+    public void changeBackgroundColor(int color) {
+        View view  = findViewById(R.id.main_layout);
+        Drawable background = view.getBackground();
+        int colorFrom = Color.TRANSPARENT;
+        if (background instanceof ColorDrawable)
+            colorFrom = ((ColorDrawable) background).getColor();
+        int colorTo = color;
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        colorAnimation.setDuration(250); // milliseconds
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                View view  = findViewById(R.id.main_layout);
+                view.setBackgroundColor((int) animator.getAnimatedValue());
+            }
+
+        });
+        colorAnimation.start();
+
+
+//        view.setBackgroundColor(color);
     }
 }
